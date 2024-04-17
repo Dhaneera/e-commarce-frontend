4f1108f889551e1a93385d26dd544d744af0d735
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-18 00:22:23 +0530

    completed home page

--- clothify/src/pages/Home.jsx
+++ clothify/src/pages/Home.jsx
@@ -23,6 +23,14 @@ import bento_m2 from '../assets/m2.png'
 import bento_m3 from '../assets/m3.png'
 import bento_m4 from '../assets/m4.png'
 import bento_m5 from '../assets/m5.png'
+import men_m1 from '../assets/c1.png'
+import women_m1 from '../assets/c2.png'
+import teens_m1 from '../assets/c3.png'
+import kids_m1 from '../assets/c4.png'
+import summer_m from '../assets/summer-m.png'
+import winter_m from '../assets/winter-m.png'
+import autumn_m from '../assets/autumn-m.png'
+import spring_m from '../assets/spring-m.png'
 
 export default function Home() {
     return (
@@ -44,7 +52,7 @@ export default function Home() {
                     <img className="pt-5 w-[97%] lg:w-[95%] md:w-[94.5%] md:h-[99%] sm:w-[93%] sm:h-[99%]  " src={bento4} alt="" />
                 </div>
 
-                <div className="2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden border 8 border-red-600 ssm:mt-5 flex flex-col justify-center items-center ssm:w-[100%] ssm:mr-[4%] ">
+                <div className="2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden ssm:mt-5 flex flex-col justify-center items-center ssm:w-[100%] ssm:mr-[4%] pl-6 ">
                     <img className="" src={bento_m1} alt="" />
                     <img className="ssm:mt-[-12%]" src={bento_m2} alt="" />
                     <img className="ssm:mt-[-12%]" src={bento_m3} alt="" />
@@ -58,22 +66,34 @@ export default function Home() {
                 <div className="  w-screen  mt-20   ">
                     <img className="ml-0 p-0 w-screen mr-[-2%]" src={category} alt="" />
                 </div>
-                <div className="flex mt-7 ml-[2%] mr-[1%]  justify-center gap-5 ssm:hidden">
+                <div className="flex mt-7 ml-[2%] mr-[1%] w-[80%]  justify-center gap-5 ssm:hidden">
                     <img className="size-[20%]" src={men} alt="" />
                     <img className="size-[20%] object-cover" src={women} alt="" />
                     <img className="size-[20%]" src={teen} alt="" />
                     <img className="size-[20%]" src={kids} alt="" />
                 </div>
+                <div className=" flex flex-col items-center 2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden ssm:mt-6 pl-5  ">
+                    <img className="w-[89%]" src={men_m1} alt="" />
+                    <img className="w-[93%] ml-3" src={women_m1} alt="" />
+                    <img className="w-[90%]" src={teens_m1} alt="" />
+                    <img className="w-[89%]" src={kids_m1} alt="" />
+                </div>
                 <div className="  w-screen mt-10   ">
                     <img className="ml-0 p-0 w-screen mr-[-2%]" src={season} alt="" />
                 </div>
-                <div className="grid grid-cols-2 border 8 border-red-700 pt-5 pl-8 gap-3 mr-2 ml-2 ssm:hidden">
+                <div className="grid grid-cols-2 pt-5 pl-8 gap-3 mr-2 ml-2 ssm:hidden">
                     <img src={summer} alt="" />
                     <img className="xl:w-[93%] lg:w-[92%] md:w-[85%] sm:w-[83%]" src={winter} alt="" />
                     <img className="mt-[-14%] xl:mt-[-6%]  xl:h-[82.3%] lg:h-[81%] w-[100%] lg:mt-[-6%] md:h-[72%] md:mt-[-2%] sm:h-[80%] sm:mt-[0] " src={autumn}  alt="" />
                     <img className=" pr-12 sm:mt-[-4%] sm:h-[83%] " src={spring} alt="" />
                 </div>
-                <div className="w-screen">
+                <div className=" 2xl:hidden xl:hidden lg:hidden md:hidden sm:hidden flex flex-col items-center mt-5 pl-7">
+                    <img className="pl-1" src={summer_m} alt="" />
+                    <img className="pl-1" src={winter_m} alt="" />
+                    <img className="ml-2 " src={autumn_m} alt="" />
+                    <img className="pl-1" src={spring_m} alt="" />
+                </div>
+                <div className="w-screen mt-10">
                     <img src={visit} alt="" />
                 </div>
             </main>
