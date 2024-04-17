7b1cfe6ff9c5072d81059d55be1a283134b3b5e0
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-04-17 10:41:34 +0530

    productPage Complete

--- clothify/src/App.jsx
+++ clothify/src/App.jsx
@@ -5,8 +5,9 @@ import Login from './pages/Login'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Error from './pages/Error'
-import ProductSelectionPage from './pages/ProductSelectionPage'
+import ProductSelectionPage from './pages/ProductSelection'
 import Cart from './pages/components/Cart'
+import Footer from './pages/components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,7 +16,7 @@ function App() {
       <Routes>
         <Route path='/'element={<Login/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
-        <Route path='/productSelect' element={<ProductSelectionPage/>}></Route>
+        <Route path='/pr' element={<ProductSelectionPage/>}></Route>
         <Route path='/signUp' element={<SignUp/>}></Route>
         <Route path='/error404' element={<Error/>}></Route>
         <Route path='/cart' element={<Cart/>}></Route>
