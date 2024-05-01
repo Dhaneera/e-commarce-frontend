b5c127a56ba9f021ae40f2c53205385b8cf46c77
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-05-01 13:12:53 +0530

    animation and response !!

--- clothify/src/pages/SubCategory.jsx
+++ clothify/src/pages/SubCategory.jsx
@@ -4,25 +4,31 @@ import frocks from '../assets/collection-frock.png'
 import denims from '../assets/collection-denim.png'
 import blouses from '../assets/collection-blouses.png'
 import perfumes from '../assets/collection-perfumes.png'
+import Header from '../pages/components/Header'
+import { motion } from 'framer-motion'
 
 export default function SubCategory() {
-    return(
+    return (
+
         <main className=' h-screen pt-8 '>
-            <div className='  gap-3 p- h-screen  grid grid-cols-2  p-10 md:hidden sm:hidden ssm:hidden '>
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={t_shirts} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={shirts} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={denims} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={frocks} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={blouses} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={perfumes} alt="" />
+            <div>
+                <Header />
             </div>
-            <div className=' 2xl:hidden xl:hidden lg:hidden p-5 gap-4 flex flex-col mt-3'>
-            <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={t_shirts} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={shirts} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={denims} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={frocks} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={blouses} alt="" />
-                <img className='w-[100%] h-[100%] object-cover rounded-3xl' src={perfumes} alt="" />
+            <div className='  gap-3 p- h-screen  grid grid-cols-2  p-20   max-lg:hidden '>
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={t_shirts} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={shirts} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={denims} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={frocks} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={blouses} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={perfumes} alt="" />
+            </div>
+            <div className=' p-16 gap-4 flex flex-col mt-3 lg:hidden'>
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={t_shirts} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={shirts} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={denims} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={frocks} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={blouses} alt="" />
+                <motion.img whileHover={{scale:1.15}} className='w-[100%] h-[100%] object-cover rounded-3xl' src={perfumes} alt="" />
             </div>
         </main>
     )
